f57a2672311920093ec79b5d0224d9e635a46c43
4O4errorrr <theogoetzinger@gmail.com>
2026-07-03 12:26:50 +0200

    LastVV

--- app.py
+++ app.py
@@ -624,4 +624,9 @@ def scoreboard():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    render_port = os.environ.get("PORT")
+    port = int(render_port or 5000)
+    host = "0.0.0.0" if render_port else "127.0.0.1"
+    debug = render_port is None
+
+    app.run(host=host, port=port, debug=debug)
